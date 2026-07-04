c85cd515218732471ef5abef153dd4b193f9ca90
Andrey <andrejverkeev99@gmail.com>
2026-07-04 06:39:11 +0300

    resolve/conflicts

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles/reset.scss';
 
 import App from './App.jsx';
+import './styles/reset.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
