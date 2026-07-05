a2bb5036de8a71c5a77bc4f0c926d3a16bf5886d
Andrey <andrejverkeev99@gmail.com>
2026-07-05 12:46:40 +0300

    add rules for esLint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import eslintConfigPrettier from "eslint-config-prettier";
-import { defineConfig, globalIgnores } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  globalIgnores(["dist"]),
+  globalIgnores(['dist']),
 
   js.configs.recommended,
   reactHooks.configs.flat.recommended,
@@ -14,7 +15,10 @@ export default defineConfig([
   eslintConfigPrettier,
 
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
+    plugins: {
+      'unused-imports': unusedImports,
+    },
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
@@ -22,6 +26,19 @@ export default defineConfig([
           jsx: true,
         },
       },
+    },
+    rules: {
+      'unused-imports/no-unused-imports': 'error',
+
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 ]);
