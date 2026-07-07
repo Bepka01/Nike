4fbc7011d205735b91d96b8e2eeff6c977f699d8
Andrey <andrejverkeev99@gmail.com>
2026-07-07 22:07:03 +0300

    adds fonts and fix index

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Typography from './components/ui/Typography';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Typography fontFamily="aeroport" variant="title-product">
+        Хуй
+      </Typography>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ui/Typography/index.jsx
+++ src/components/ui/Typography/index.jsx
@@ -1,9 +1,22 @@
 import style from './style.module.scss';
 import cn from 'classnames';
 
-const Typography = ({ tag: Tag = 'p', variant, className, children }) => {
+const Typography = ({
+  tag: Tag = 'p',
+  fontFamily,
+  variant,
+  className = 'aeroport',
+  children,
+}) => {
   return (
-    <Tag className={cn(style.typography, style[variant], className)}>
+    <Tag
+      className={cn(
+        style.typography,
+        style[variant],
+        style[fontFamily],
+        className
+      )}
+    >
       {children}
     </Tag>
   );
