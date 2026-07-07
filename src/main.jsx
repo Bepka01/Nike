f6454008aa465880a130c32b301da84ed94e8c09
Andrey <andrejverkeev99@gmail.com>
2026-07-07 22:35:34 +0300

    add-variables

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles/reset.scss';
+import './assets/fonts.scss';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
