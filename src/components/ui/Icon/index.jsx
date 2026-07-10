8c982b1ffcaaa5050e0d3bf93d7535432f2abd77
Andrey <andrejverkeev99@gmail.com>
2026-07-11 01:50:30 +0300

    feature/Icon

---
+++ src/components/ui/Icon/index.jsx
@@ -0,0 +1,14 @@
+import glass from '../../../assets/icons/glass.svg';
+import trash from '../../../assets/icons/trash (2).svg';
+import arrow from '../../../assets/icons/arrow.svg';
+import vk from '../../../assets/icons/vk.svg';
+import inst from '../../../assets/icons/instagram.svg';
+import faceBook from '../../../assets/icons/facebook 1 (1).svg';
+
+const icons = [glass, trash, arrow, vk, inst, faceBook];
+
+const Icon = ({ name, alt = '', ...props }) => {
+  return <img src={icons[name]} alt={alt} {...props} />;
+};
+
+export default Icon;
