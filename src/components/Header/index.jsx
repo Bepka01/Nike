e2a2c75e915ba60b08d5330c8ac6728f5caf1169
Andrey <andrejverkeev99@gmail.com>
2026-07-19 01:07:21 +0300

    addedBurger and fix fonts

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,6 +5,7 @@ import style from './style.module.scss';
 
 const Header = () => {
   const [activeMenu, setActiveMenu] = useState(null);
+  const [activeBurger, setBurger] = useState(false);
 
   const toggleMenu = (menu) => {
     if (activeMenu === menu) {
@@ -15,79 +16,97 @@ const Header = () => {
   };
 
   return (
-    <header className={style.header}>
-      <div className={style.headerLeft}>
-        <Icon name="nike" color="black" className={style.logoDesktop} />
+    <>
+      <header className={style.header}>
+        <div className={style.headerLeft}>
+          <Icon name="nike" color="black" className={style.logoDesktop} />
 
-        <div className={style.burgerMenu}>
-          <div className={style.burgerLine}></div>
-          <div className={style.burgerLine}></div>
-          <div className={style.burgerLine}></div>
+          <div
+            onClick={() => {
+              setBurger(!activeBurger);
+              console.log(activeBurger);
+            }}
+            className={`${style.burgerMenu} ${activeBurger ? style.burgerOpen : ''}`}
+          >
+            <div className={style.burgerLine}></div>
+            <div className={style.burgerLine}></div>
+            <div className={style.burgerLine}></div>
+          </div>
         </div>
-      </div>
 
-      <div className={style.headerCenter}>
-        <Icon name="nike" color="black" className={style.logoMobile} />
+        <div className={style.headerCenter}>
+          <Icon name="nike" color="black" className={style.logoMobile} />
 
-        <nav className={style.navigation}>
-          <div className={style.menuItem}>
-            <Typography
-              variant="header-menu"
-              onClick={() => toggleMenu('clothes')}
-            >
-              Одежда
-            </Typography>
+          <nav className={style.navigation}>
+            <div className={style.menuItem}>
+              <Typography
+                variant="header-menu"
+                onClick={() => toggleMenu('clothes')}
+              >
+                Одежда
+              </Typography>
 
-            {activeMenu === 'clothes' && (
-              <div className={style.dropdown}>
-                <p>Кроссовки</p>
-                <p>Футболки</p>
-                <p>Шорты</p>
-              </div>
-            )}
-          </div>
+              {activeMenu === 'clothes' && (
+                <div className={style.dropdown}>
+                  <p>Кроссовки</p>
+                  <p>Футболки</p>
+                  <p>Шорты</p>
+                </div>
+              )}
+            </div>
 
-          <div className={style.menuItem}>
-            <Typography
-              variant="header-menu"
-              onClick={() => toggleMenu('accessories')}
-            >
-              Аксессуары
-            </Typography>
+            <div className={style.menuItem}>
+              <Typography
+                variant="header-menu"
+                onClick={() => toggleMenu('accessories')}
+              >
+                Аксессуары
+              </Typography>
 
-            {activeMenu === 'accessories' && (
-              <div className={style.dropdown}>
-                <p>Рюкзаки</p>
-                <p>Носки</p>
-                <p>Кепки</p>
-              </div>
-            )}
-          </div>
+              {activeMenu === 'accessories' && (
+                <div className={style.dropdown}>
+                  <p>Рюкзаки</p>
+                  <p>Носки</p>
+                  <p>Кепки</p>
+                </div>
+              )}
+            </div>
 
-          <div className={style.menuItem}>
-            <Typography
-              variant="header-menu"
-              onClick={() => toggleMenu('collections')}
-            >
-              Коллекции
-            </Typography>
+            <div className={style.menuItem}>
+              <Typography
+                variant="header-menu"
+                onClick={() => toggleMenu('collections')}
+              >
+                Коллекции
+              </Typography>
 
-            {activeMenu === 'collections' && (
-              <div className={style.dropdown}>
-                <p>Jordan</p>
-                <p>Air Max</p>
-                <p>Running</p>
-              </div>
-            )}
-          </div>
-        </nav>
-      </div>
+              {activeMenu === 'collections' && (
+                <div className={style.dropdown}>
+                  <p>Jordan</p>
+                  <p>Air Max</p>
+                  <p>Running</p>
+                </div>
+              )}
+            </div>
+          </nav>
+        </div>
 
-      <div className={style.headerRight}>
-        <Icon name="glass" className={style.search} />
-        <Icon name="trash" />
-      </div>
-    </header>
+        <div className={style.headerRight}>
+          <Icon name="glass" className={style.search} />
+          <Icon name="trash" />
+        </div>
+      </header>
+
+      {activeBurger && (
+        <div className={style.burgerNavigation}>
+          <Typography className={style.headerMenuMobile} variant="header-menu">
+            Одежда
+          </Typography>
+          <Typography variant="header-menu">Аксессуары</Typography>
+          <Typography variant="header-menu">Коллекции</Typography>
+        </div>
+      )}
+    </>
   );
 };
 
