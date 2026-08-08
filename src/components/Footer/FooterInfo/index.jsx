ff03564e78beab349cb87c272f0c4f37f42394d1
Andrey <andrejverkeev99@gmail.com>
2026-08-08 07:15:37 +0300

    fix/footer

--- src/components/Footer/FooterInfo/index.jsx
+++ src/components/Footer/FooterInfo/index.jsx
@@ -1,20 +1,16 @@
 import { Link } from 'react-router-dom';
-import { footerColumns } from '../../../constants';
 import style from './style.module.scss';
 
 const FooterInfo = () => {
-  const info = footerColumns.find((item) => item.path === 'info');
-
   return (
     <div className={style.footerInfo}>
       <p>© Nike, Inc., 2020. Все права защищены.</p>
 
       <div className={style.links}>
-        {info.links.map(({ title, path }) => (
-          <Link key={path} to={`/footer/${info.path}/${path}`}>
-            {title}
-          </Link>
-        ))}
+        <Link to="/catalog">Каталог</Link>
+        <Link to="/terms">Условия использования</Link>
+        <Link to="/sales">Условия продажи</Link>
+        <Link to="/privacy">Конфиденциальность и файлы cookie</Link>
       </div>
     </div>
   );
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -5,7 +5,6 @@ import { ROUTES } from './routes';
 import Main from '../pages/main/index';
 import Trash from '../pages/trash/index';
 import Catalog from '../pages/catalog/index';
-import FooterPage from '../pages/footer';
 
 const AppRoutes = () => {
   return (
@@ -13,7 +12,6 @@ const AppRoutes = () => {
       <Route path={ROUTES.MAIN} element={<Main />} />
       <Route path={ROUTES.TRASH} element={<Trash />} />
       <Route path="/:category/:subcategory" element={<Catalog />} />
-      <Route path="/footer/:aboutTitle/:page" element={<FooterPage />} />
     </Routes>
   );
 };
