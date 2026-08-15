7a4ed56b63c0cabe7f9d09d77581a3c3d279ec9f
Andrey <andrejverkeev99@gmail.com>
2026-08-16 01:04:57 +0300

    added/viewedProducts

--- src/components/ViewedProducts/index.jsx
+++ src/components/ViewedProducts/index.jsx
@@ -2,20 +2,49 @@ import Typography from '../ui/Typography/index';
 import ViewedProductCard from './viewedProductCard';
 import style from './style.module.scss';
 import Icon from '../ui/Icon/index';
+import { useState } from 'react';
 
 const ViewedProducts = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const handleNext = () => {
+    setCurrentIndex((prev) => prev + 1);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prev) => prev - 1);
+  };
+
   return (
     <div className={style.viewedProducts}>
       <div className={style.viewedProductsHeader}>
         <Typography variant="small-title">История просмотров</Typography>
 
         <div className={style.navigation}>
-          <Icon name="dropDownArrow" />
-          <Icon name="dropDownArrow" color="black" />
+          <Icon
+            onClick={handlePrev}
+            name="dropDownArrow"
+            className={style.left}
+          />
+          <Icon
+            onClick={handleNext}
+            name="dropDownArrow"
+            className={style.right}
+          />
         </div>
       </div>
 
       <div className={style.productsContainer}>
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
+        <ViewedProductCard />
         <ViewedProductCard />
         <ViewedProductCard />
         <ViewedProductCard />
--- src/components/ViewedProducts/viewedProductCard/index.jsx
+++ src/components/ViewedProducts/viewedProductCard/index.jsx
@@ -1,9 +1,10 @@
 import style from './style.module.scss';
+import photo from '../../../assets/img/viewedCard.png';
 
 const ViewedProductCard = () => {
   return (
-    <div>
-      <img src="" alt="" />
+    <div className={style.viewedProductCard}>
+      <img src={photo} alt="foto" />
       <div className={style.aboutCard}>
         <p className={style.titleCard}>Nike Air Max 270 React ENG</p>
         <p className={style.PriceCard}>2320 ₽</p>
