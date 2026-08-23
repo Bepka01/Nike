8d4f9c5ed6b23143f22b0b761d53685d333f23e6
Andrey <andrejverkeev99@gmail.com>
2026-08-23 10:51:12 +0300

    add btn to product

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,5 +1,14 @@
+import { Link } from 'react-router-dom';
+
 const Main = () => {
-  return <h1>Главная страница</h1>;
+  return (
+    <>
+      <h1>Главная страница</h1>
+      <Link to="/product">
+        <button>К товару</button>
+      </Link>
+    </>
+  );
 };
 
 export default Main;
